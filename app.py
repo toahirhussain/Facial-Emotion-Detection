d2ccad20728858b131020813cb2d354c4116338d
Toahir Hussain <toahirhussain222@gmail.com>
2025-12-16 23:04:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ if uploaded is not None:
           """,
           unsafe_allow_html=True
         )
-
+    st.divider()
     # Button (functionality)
     tell_joke = st.button("Tell me a joke 😄")
 
